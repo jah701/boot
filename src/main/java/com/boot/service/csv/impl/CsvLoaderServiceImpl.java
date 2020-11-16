579d6d78e390f2470fe1f77367a3c12a7436a7aa
Jah <moskovienkor@gmail.com>
2020-11-16 12:16:12 +0200

    sb-01: - done: * CSVLoader implemented (read file from url); * SecurityConfiguration added;

---
+++ src/main/java/com/boot/service/csv/impl/CsvLoaderServiceImpl.java
@@ -0,0 +1,26 @@
+package com.boot.service.csv.impl;
+
+import com.boot.service.csv.CsvLoaderService;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CsvLoaderServiceImpl implements CsvLoaderService {
+    @Override
+    public boolean loadCsvFile(String url, String intoPath) {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(intoPath)) {
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't load CSV file from " + url, e);
+        }
+        return true;
+    }
+}
